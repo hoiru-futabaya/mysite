9bb1a7cc4da63053dc558f26a5988729d30ee1e0
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-11-24 22:56:10 +0900

    リンクページ作成とgetIp.jsにbabel

--- getIp.js
+++ getIp.js
@@ -2,17 +2,13 @@ fetch('https://ipinfo.io/json', {
   credentials: 'same-origin',
   headers: {
     'Content-Type': 'application/json'
-  }}
-).then(
-		response => response.json()
-).then(
-  ip => {
-    fetch('https://script.google.com/macros/s/AKfycbwMZwR-7TSCJ79fgfc8yiU37at5fpt6wcVDMTI7Weik-bYqxBaH/exec?name=index&ip='+ip.ip)
-				.then(response => {
-				return response.text();
-				})
-				.then(response => {
-				document.getElementById('getCounter').textContent = JSON.parse(response).count;
-				})
-})
-
+  }
+}).then(function (response) {
+  return response.json();
+}).then(function (ip) {
+  fetch('https://script.google.com/macros/s/AKfycbwMZwR-7TSCJ79fgfc8yiU37at5fpt6wcVDMTI7Weik-bYqxBaH/exec?name=index&ip=' + ip.ip).then(function (response) {
+    return response.text();
+  }).then(function (response) {
+    document.getElementById('getCounter').textContent = JSON.parse(response).count;
+  });
+});
